942bac94f4a134655507f00e64d07f5747cc4ecd
Cyril Cecchinel <ulrich06@users.noreply.github.com>
2017-01-20 17:56:46 +0100

    Arduino code

---
+++ oscilloscope/oscilloscope.ino
@@ -0,0 +1,54 @@
+#define LED_GREEN 8
+#define LED_RED 9
+#define LED_BLUE 10
+#define LED_A    3
+#define CHANNEL_1 5
+#define CHANNEL_2 6
+#define CHANNEL_3 7
+
+#define CHANNEL_A 0
+
+#define DELAY 200
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(CHANNEL_1, INPUT);
+  pinMode(CHANNEL_2, INPUT);
+  pinMode(CHANNEL_3, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int channel_1 = digitalRead(CHANNEL_1);
+  int channel_2 = digitalRead(CHANNEL_2);
+  int channel_3 = digitalRead(CHANNEL_3);
+
+  int channel_a = analogRead(CHANNEL_A);
+  
+  digitalWrite(LED_GREEN, channel_1);
+  output("channel_1", channel_1);
+  
+  digitalWrite(LED_RED, channel_2);
+  output("channel_2", channel_2);
+   
+  digitalWrite(LED_BLUE, channel_3);
+  output("channel_3", channel_3);
+
+  analogWrite(LED_A, channel_a);
+  output("channel_a", channel_a);
+  
+  delay(DELAY);
+  
+}
+
+void output(String channel, int value){
+  float t = millis() / 1000.0;
+  Serial.println("{\"channel\":\"" + channel + "\", \"value\":" + String(value) + ",\"time\":" + String(t) + "}"); 
+}
+
+
